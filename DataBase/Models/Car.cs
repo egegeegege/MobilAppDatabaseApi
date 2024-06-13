cb2818036314619628c078043d4b1f3494754a1f
ege aydın <egeaydinn31@gmail.com>
2024-06-13 13:45:54 +0300

    Proje dosyası ekle.

---
+++ DataBase/Models/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace DataBase.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public long TcNo { get; set; }
+        public int Sifre { get; set; }
+    }
+}
