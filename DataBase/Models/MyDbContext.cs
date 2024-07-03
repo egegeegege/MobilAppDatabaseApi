d12b71c740a18531234827ad0db547f4c309728c
ege aydın <egeaydinn31@gmail.com>
2024-07-03 15:04:25 +0300

    teacher final Api

--- DataBase/Models/MyDbContext.cs
+++ DataBase/Models/MyDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataBase.Models
 {
@@ -48,44 +49,43 @@ namespace DataBase.Models
 
             modelBuilder.Entity<Teacher>().HasData(
 
-                new Teacher { Id = 1, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 2, Ad = "Meltem", Soyad = "Karadağ", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 3, Ad = "Pelin", Soyad = "Şendil", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 4, Ad = "Harun", Soyad = "Gürsoy", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 5, Ad = "Aslı", Soyad = "Gürel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 6, Ad = "Necmettin Alp", Soyad = "Ar", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 7, Ad = "Ebru", Soyad = "Şahin", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 8, Ad = "Özlem", Soyad = "Bayram", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 9, Ad = "Sinan", Soyad = "Vural", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 10, Ad = "Celil", Soyad = "Öztürk", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 11, Ad = "Barış", Soyad = "Bozan", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 12, Ad = "Güneş", Soyad = "Parlakgün", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 13, Ad = "Emel", Soyad = "Pülent", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 14, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 15, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 16, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 17, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 18, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 19, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 20, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 21, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 22, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 23, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 24, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 25, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 26, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 27, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 28, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 29, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 30, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 31, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 32, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 33, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 34, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 35, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 36, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 37, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
-                new Teacher { Id = 38, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" }
+                new Teacher { Id = 1, Ad = "Ömer", Soyad = "Yüksel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür", Cinsiyet = "Erkek", OturduguIlce = "Beyoğlu", Aktif = "Hayır" },
+                new Teacher { Id = 2, Ad = "Meltem", Soyad = "Karadağ", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür Yardımcısı", Cinsiyet = "Kadın", OturduguIlce = "Kadıköy", Aktif = "Evet" },
+                new Teacher { Id = 3, Ad = "Pelin", Soyad = "Şendil", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müdür Yardımcısı", Cinsiyet = "Kadın", OturduguIlce = "Kadıköy", Aktif = "Evet" },
+                new Teacher { Id = 4, Ad = "Harun", Soyad = "Gürsoy", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Bilişim Teknolojileri Öğretmeni", Cinsiyet = "Erkek", OturduguIlce = "Kadıköy", Aktif = "Evet" },
+                new Teacher { Id = 5, Ad = "Aslı", Soyad = "Gürel", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Bilişim Teknolojileri Öğretmeni", Cinsiyet = "Kadın", OturduguIlce = "Kadıköy", Aktif = "Evet" },
+                new Teacher { Id = 6, Ad = "Necmettin Alp", Soyad = "Ar", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Bilişim Teknolojileri Öğretmeni", Cinsiyet = "Erkek", OturduguIlce = "Sancaktepe", Aktif = "Evet" },
+                new Teacher { Id = 7, Ad = "Ebru", Soyad = "Şahin", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Bilişim Teknolojileri Öğretmeni", Cinsiyet = "Kadın", OturduguIlce = "Kadıköy", Aktif = "Evet" },
+                new Teacher { Id = 8, Ad = "Özlem", Soyad = "Bayram", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Bilişim Teknolojileri Öğretmeni", Cinsiyet = "Kadın", OturduguIlce = "Kadıköy", Aktif = "Evet" },
+                new Teacher { Id = 9, Ad = "Sinan", Soyad = "Vural", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Bilişim Teknolojileri Öğretmeni", Cinsiyet = "Erkek", OturduguIlce = "Ataşehir", Aktif = "Evet" },
+                new Teacher { Id = 10, Ad = "Celil", Soyad = "Öztürk", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Bilişim Teknolojileri Öğretmeni", Cinsiyet = "Erkek", OturduguIlce = "Ataşehir", Aktif = "Evet" },
+                new Teacher { Id = 11, Ad = "Barış", Soyad = "Bozan", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Bilişim Teknolojileri Öğretmeni", Cinsiyet = "Erkek", OturduguIlce = "Ataşehir", Aktif = "Evet" },
+                new Teacher { Id = 12, Ad = "Güneş", Soyad = "Parlakgün", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Bilişim Teknolojileri Öğretmeni", Cinsiyet = "Kadın", OturduguIlce = "Kadıköy", Aktif = "Evet" },
+                new Teacher { Id = 13, Ad = "Emel", Soyad = "Pülent", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Halkla İlişkiler ve Organizasyon Hizmetleri", Cinsiyet = "Kadın", OturduguIlce = "Ümraniye", Aktif = "Evet" },
+                new Teacher { Id = 14, Ad = "Aslı", Soyad = "Taban", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Halkla İlişkiler ve Organizasyon Hizmetleri", Cinsiyet = "Kadın", OturduguIlce = "Ümraniye", Aktif = "Evet" },
+                new Teacher { Id = 15, Ad = "Gökhan", Soyad = "Güngör", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Halkla İlişkiler ve Organizasyon Hizmetleridür", Cinsiyet = "Erkek", OturduguIlce = "Kadıköy", Aktif = "Evet" },
+                new Teacher { Id = 16, Ad = "Yunus Kaan", Soyad = "Özkan", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Halkla İlişkiler ve Organizasyon Hizmetleri", Cinsiyet = "Erkek", OturduguIlce = "Kadıköy", Aktif = "Evet" },
+                new Teacher { Id = 17, Ad = "Gülay Tokgöz", Soyad = "Kanmaz", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Edebiyat Öğretmeni", Cinsiyet = "Erkek", OturduguIlce = "Ümraniye", Aktif = "HaEvetyır" },
+                new Teacher { Id = 18, Ad = "Nazife", Soyad = "Kaçar", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Edebiyat Öğretmeni", Cinsiyet = "Kadın", OturduguIlce = "Kadıköy", Aktif = "Evet" },
+                new Teacher { Id = 19, Ad = "Özlem", Soyad = "Koşal", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Edebiyat Öğretmeni", Cinsiyet = "Kadın", OturduguIlce = "Kadıköy", Aktif = "Evet" },
+                new Teacher { Id = 20, Ad = "Sema Deniz", Soyad = "Yolsal", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Edebiyat Öğretmeni", Cinsiyet = "Kadın", OturduguIlce = "Sancaktepe", Aktif = "Hayır" },
+                new Teacher { Id = 21, Ad = "Dilek", Soyad = "Erbil", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Matematik Öğretmeni", Cinsiyet = "Kadın", OturduguIlce = "Kadıköy", Aktif = "Evet" },
+                new Teacher { Id = 22, Ad = "Işık Kutlu", Soyad = "Çalışkan", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Matematik Öğretmeni", Cinsiyet = "Kadın", OturduguIlce = "Ümraniye", Aktif = "Evet" },
+                new Teacher { Id = 23, Ad = "Züleyha", Soyad = "Atmaca", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Tarih Öğretmeni", Cinsiyet = "Kadın", OturduguIlce = "Kadıköy", Aktif = "Evet" },
+                new Teacher { Id = 24, Ad = "Zehra", Soyad = "Taşçı", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Tarih Öğretmeni", Cinsiyet = "Kadın", OturduguIlce = "Kadıköy", Aktif = "Evet" },
+                new Teacher { Id = 25, Ad = "Nejla", Soyad = "Yılmaz", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Coğrafya Öğretmeni", Cinsiyet = "Kadın", OturduguIlce = "Üsküdar", Aktif = "Evet" },
+                new Teacher { Id = 26, Ad = "Serpil", Soyad = "Gültepe", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Fizik Öğretmeni", Cinsiyet = "Kadın", OturduguIlce = "Ataşehir", Aktif = "Evet" },
+                new Teacher { Id = 27, Ad = "Merve", Soyad = "Tamtürk", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Kimya Öğretmeni", Cinsiyet = "Kadın", OturduguIlce = "Üsküdar", Aktif = "Evet" },
+                new Teacher { Id = 28, Ad = "Engin", Soyad = "Keklil", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Biyoloji Öğretmeni", Cinsiyet = "Erkek", OturduguIlce = "Üsküdar", Aktif = "Evet" },
+                new Teacher { Id = 29, Ad = "Şeyda Gülsün", Soyad = "Tangör", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "İngilizce Öğretmeni", Cinsiyet = "Kadın", OturduguIlce = "Üsküdar", Aktif = "Evet" },
+                new Teacher { Id = 30, Ad = "Hande Fatma", Soyad = "Ocak", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "İngilizce Öğretmeni", Cinsiyet = "Kadın", OturduguIlce = "Kadıköy", Aktif = "Evet" },
+                new Teacher { Id = 31, Ad = "Hatice Handan", Soyad = "Şimşek", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Felsefe Öğretmeni", Cinsiyet = "Kadın", OturduguIlce = "Kadıköy", Aktif = "Evet" },
+                new Teacher { Id = 32, Ad = "Muteber", Soyad = "Demirkale", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Beden Eğitimi Öğretmeni", Cinsiyet = "Kadın", OturduguIlce = "Ümraniye", Aktif = "Evet" },
+                new Teacher { Id = 33, Ad = "Süheyla", Soyad = "Subay", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Müzik Öğretmeni", Cinsiyet = "Kadın", OturduguIlce = "Kadıköy", Aktif = "Evet" },
+                new Teacher { Id = 34, Ad = "Esma", Soyad = "Kaymak", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Din Kült. ve Ahl.Bil. Öğretmeni", Cinsiyet = "Kadın", OturduguIlce = "Ataşehir", Aktif = "Evet" },
+                new Teacher { Id = 35, Ad = "Hasan", Soyad = "Yeşilyurt", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Din Kült. ve Ahl.Bil. Öğretmeni", Cinsiyet = "Erkek", OturduguIlce = "Kadıköy", Aktif = "Evet" },
+                new Teacher { Id = 36, Ad = "Songül", Soyad = "Ataç", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Psikolojik Danışman", Cinsiyet = "Kadın", OturduguIlce = "Üsküdar", Aktif = "Evet" },
+                new Teacher { Id = 37, Ad = "Zeynep", Soyad = "Biter", TelefonNo = "+90 5__ ___ __ __", UzmanlıkAlanıDersler = "Psikolojik Danışman", Cinsiyet = "Kadın", OturduguIlce = "Kadıköy", Aktif = "Evet" }
             );
 
 
