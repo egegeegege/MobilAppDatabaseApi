36d19c14ef29371b34ecf6ece8c9f793f0f10192
ege aydın <egeaydinn31@gmail.com>
2024-07-03 16:02:15 +0300

    .

--- DataBase/Models/Teacher.cs
+++ DataBase/Models/Teacher.cs
@@ -4,6 +4,7 @@
     {
         public string Ad { get; set; }
         public string Soyad { get; set; }
+        public string Sifre { get; set; }
         public string TelefonNo { get; set; }
         public string OturduguIlce { get; set; }
         public string UzmanlıkAlanıDersler { get; set; }
