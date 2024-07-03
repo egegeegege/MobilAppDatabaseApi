b6e530618564278cec6d54eecab8aaadf634513a
ege aydın <egeaydinn31@gmail.com>
2024-07-03 16:35:55 +0300

    .

--- DataBase/Models/BaseEntitiy.cs
+++ DataBase/Models/BaseEntitiy.cs
@@ -2,7 +2,9 @@
 
 namespace DataBase.Models
 {
-    public abstract class BaseEntitiy
+    public abstract class BaseEntitiy //Diğer model sınıflarına miras vermek için bir base sınıf oluşturdum bu sınıfı hangi sınıfa miras verirsek bu sınıfın içindeki herşey
+                                      //Miras vediğimiz sınıfada geçecektik bunu yapmamız için bu sınıfı abstract olarak işaretlememizde gerekir burdaki nilgileri diğer sınıflara 
+                                      //defalarca yazmak istemediğim için miras alma yöntemini kullandım umarım anlamışsınızdır.
     {
         public int Id { get; set; }
         public DateTime? AddDate { get; set; }
